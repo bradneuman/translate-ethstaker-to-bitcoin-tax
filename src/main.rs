962923897900cc514c179d2c1f0590268078f16c
Brad Neuman <bradneuman@Brads-MacBook-Pro.local>
2024-04-04 13:23:14 -0700

    Initial working code to read csvs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,81 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io, process};
+// use serde::Deserialize;
+
+/// For rocketpool there are additional columns but the initial colums are all the same.
+const SHARED_COLUMN_NAMES: [&'static str; 5] = [
+    "Date",
+    "Validator Index",
+    "Price [USD/ETH]",
+    "Consensus Layer Income [ETH]",
+    "Execution Layer Income [ETH]",
+];
+const ROCKETPOOL_EXTRA_COLUMN_NAMES: [&'static str; 4] = [
+    "Node Address",
+    "Smoothing Pool Income [ETH]",
+    "Rocket Pool Node Income [RPL]",
+    "Price [USD/RPL]",
+];
+
+#[derive(Debug, serde::Deserialize)]
+struct Row {
+    // TODO:(bn) Date type
+    #[serde(rename = "Date")]
+    date: String,
+
+    #[serde(rename = "Validator Index")]
+    validator_index: Option<u64>,
+
+    // TODO:(bn) fixed point numbers
+    // TODO:(bn) strong types?
+    #[serde(rename = "Consensus Layer Income [ETH]")]
+    consensus_income_eth: Option<f64>,
+
+    #[serde(rename = "Execution Layer Income [ETH]")]
+    execution_income_eth: Option<f64>,
+
+    #[serde(rename = "Smoothing Pool Income [ETH]")]
+    smothing_pool_income_eth: Option<f64>,
+
+    #[serde(rename = "Rocket Pool Node Income [RPL]")]
+    node_income_rpl: Option<f64>,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_reader(io::stdin());
+
+    {
+        let headers = rdr.headers()?;
+
+        // TODO:(bn) errors instead of panic?
+        let solo_num_columns = SHARED_COLUMN_NAMES.len();
+        let rpl_num_columns = SHARED_COLUMN_NAMES.len() + ROCKETPOOL_EXTRA_COLUMN_NAMES.len();
+
+        assert!(
+            headers.len() == solo_num_columns || headers.len() == rpl_num_columns,
+            "Invalid number of columns"
+        );
+        for (i, v) in headers.iter().enumerate() {
+            if i < SHARED_COLUMN_NAMES.len() {
+                assert_eq!(v, SHARED_COLUMN_NAMES[i]);
+            } else {
+                let i = i - SHARED_COLUMN_NAMES.len();
+                assert_eq!(v, ROCKETPOOL_EXTRA_COLUMN_NAMES[i]);
+            }
+        }
+    }
+
+    for result in rdr.deserialize() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record: Row = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    example()
 }
